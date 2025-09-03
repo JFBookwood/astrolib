b4af05debf4e869e18c05cff564f79b909d8fab6
JFBookwood <jesse.buchholz@jgmm.de>
2025-09-03 15:56:57 +0200

    Version 0.2.0: Implementiere PMNS-Matrix und Watershed Void Finder

--- astrolib/neutrino.py
+++ astrolib/neutrino.py
@@ -24,8 +24,29 @@ def pmns_matrix(theta12, theta23, theta13, delta_cp):
     Returns:
         np.ndarray: PMNS-Matrix
     """
-    # Implementation folgt
-    pass
+    # Berechne trigonometrische Funktionen
+    s12 = np.sin(theta12)
+    s23 = np.sin(theta23)
+    s13 = np.sin(theta13)
+    c12 = np.cos(theta12)
+    c23 = np.cos(theta23)
+    c13 = np.cos(theta13)
+    
+    # Initialisiere die PMNS-Matrix
+    U = np.zeros((3, 3), dtype=np.complex128)
+    
+    # Fülle die Matrix entsprechend der PMNS-Parametrisierung
+    U[0, 0] = c12 * c13
+    U[0, 1] = s12 * c13
+    U[0, 2] = s13 * np.exp(-1j * delta_cp)
+    U[1, 0] = -s12 * c23 - c12 * s23 * s13 * np.exp(1j * delta_cp)
+    U[1, 1] = c12 * c23 - s12 * s23 * s13 * np.exp(1j * delta_cp)
+    U[1, 2] = s23 * c13
+    U[2, 0] = s12 * s23 - c12 * c23 * s13 * np.exp(1j * delta_cp)
+    U[2, 1] = -c12 * s23 - s12 * c23 * s13 * np.exp(1j * delta_cp)
+    U[2, 2] = c23 * c13
+    
+    return U
 
 class NeutrinoOscillation:
     """
